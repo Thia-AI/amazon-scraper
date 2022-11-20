60054ccae73f6c4788482c2813a9dbde41a6ce3b
lasitha-amuwala <lasithaamuwala@gmail.com>
2022-11-20 03:32:17 -0500

    Added scraping multiple review pages of 1 product

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import math
+
 import requests
 import pandas as pd
 from tabulate import tabulate
@@ -5,6 +7,8 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+
+MAX_REVIEWS_PAGES_PER_PRODUCT = 10;
 
 baseURL = "https://www.amazon.ca"
 headers = ({
@@ -31,12 +35,7 @@ def scrape_rendered_page(url):
     return soup;
 
 
-def scrape_reviews_page(url, reviews):
-    print("Scraping reviews list page...")
-    soup2 = scrape_page(url)
-    results2 = soup2.find(id="cm_cr-review_list")
-    review_cards = results2.find_all("div", {'data-hook': 'review'})
-
+def scrape_reviews(review_cards, reviews):
     for count, card in enumerate(review_cards, start=1):
         print("Scraping review ", count, "/", len(review_cards), "...")
         profile_link = card.find("a", class_='a-profile')
@@ -73,21 +72,40 @@ def scrape_reviews_page(url, reviews):
 
 def main():
     try:
-        print("Scraping product page for reviews list page...")
         url = "https://www.amazon.ca/dp/1789955750/ref=sspa_dk_detail_3?psc=1&pd_rd_i=1789955750&pd_rd_w=ysA69&content-id=amzn1.sym.c7dca932-da6a-44fc-af09-cc68d2449b34&pf_rd_p=c7dca932-da6a-44fc-af09-cc68d2449b34&pf_rd_r=BC37YMSSJZ77JTMS3HA3&pd_rd_wg=utjBQ&pd_rd_r=a58b079a-8110-4850-a1c3-c5da0ad68ba1&s=books&sp_csd=d2lkZ2V0TmFtZT1zcF9kZXRhaWw"
+        print("Scraping product page for reviews list page...  ", url)
         soup = scrape_page(url)
 
         results = soup.find(id="cr-pagination-footer-0")
+        print("Reviews list page found")
+
         link_url = results.find("a", class_="a-link-emphasis a-text-bold")['href']
         new_url = baseURL + link_url
     except:
         print("ERROR: User-Agent Expired, try another: https://developers.whatismybrowser.com/useragents/explore/")
         return 0
 
-    print("Reviews list page found")
-    reviews = []
-    reviews = scrape_reviews_page(new_url, reviews)
+    print("Scraping reviews list page...")
+    soup2 = scrape_page(new_url)
 
+    # find number of pages to scrape in reviews list, maximum 10 pages (100 reviews)
+    total_num_reviews = \
+        soup2.find("div", {'data-hook': 'cr-filter-info-review-rating-count'}).text.split(', ')[1].split(" ")[0]
+    total_num_reviews = int(total_num_reviews)
+    number_of_review_pages_to_scrape = math.ceil(
+        total_num_reviews / MAX_REVIEWS_PAGES_PER_PRODUCT) if total_num_reviews < MAX_REVIEWS_PAGES_PER_PRODUCT * 10 else 10
+
+    # go through the review pages and scrape the review data
+    reviews = []
+    for page_number in range(1, number_of_review_pages_to_scrape + 1):
+        pageUrl = new_url + '&pageNumber=' + str(page_number)
+        print("Scaping page number ", page_number, "...   ", pageUrl)
+        soup3 = scrape_page(pageUrl)
+        review_list = soup3.find(id="cm_cr-review_list")
+        review_cards = review_list.find_all("div", {'data-hook': 'review'})
+        reviews = scrape_reviews(review_cards, reviews)
+
+    print("Scraping Finished, converting data into cool tabular view...")
     df = pd.DataFrame(reviews)
     print(tabulate(df, headers='keys', tablefmt='github'))
 
@@ -95,5 +113,3 @@ def main():
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
